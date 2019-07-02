f008b80cb4d92ef4856bcaa0b7ddf79dd5adae46
digitalbricks <mail@andreherdling.de>
2019-07-02 21:09:23 +0200

    v0.2, following multiple redirects

---
+++ changelog.md
@@ -0,0 +1,4 @@
+# Changelog simplelinkchecker
+
+## v0.2, 02.07.2019
+– now following multiple redirects and showing url and status for each redirect target
--- proxy.php
+++ proxy.php
@@ -7,6 +7,26 @@ echo json_encode(getHttpStatus($url));
 
 
 function getHttpStatus($url){
+    $status = doRequest($url);
+    
+    // if we got an redirect, follow until there is no redirect any more
+    if($status['status'] == 302 OR $status['status'] == 301){
+        $status_new = $status;
+        while($status_new['status']==302 OR $status_new['status']==301){
+            $status_new = doRequest($status_new['target']);
+            $httpstatuscode = $status_new['status'];
+            if($httpstatuscode==302 OR $httpstatuscode==301){
+                $status['target'].=" <span class='inline-status code_{$httpstatuscode}'>{$httpstatuscode}</span> &rarr;<br />".$status_new['target'];
+            } else {
+                $status['target'].=" <span class='inline-status final-status code_{$httpstatuscode}'>{$httpstatuscode}</span>";
+            }
+            
+        }
+    }
+    return $status;
+}
+
+function doRequest($url){
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
     curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
